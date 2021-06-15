3278f0d3a9983c5803604c939d251ec43c3bd2aa
Daniel Ahn <iamdanahn@gmail.com>
2021-06-15 15:18:34 -0400

    Add notes to index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 // import * as serviceWorker from "./serviceWorker";
 
 // 1
@@ -12,7 +13,7 @@ import {
 	InMemoryCache,
 } from "@apollo/client";
 
-// 2
+// 2 - links the ApolloClient instance to GraphQL API
 const httpLink = createHttpLink({
 	uri: "http://localhost:4000",
 });
@@ -25,9 +26,11 @@ const client = new ApolloClient({
 
 // 4
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<App />
-	</ApolloProvider>,
+	<BrowserRouter>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</BrowserRouter>,
 	document.getElementById("root")
 );
 // serviceWorker.unregister();
